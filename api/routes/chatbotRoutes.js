41136b37410ac3145ab6d38993d1e9400601bc05
Miikka Alaperä <miikka.alapera@student.oulu.fi>
2018-04-16 02:08:02 +0300

    Database file added

--- api/routes/chatbotRoutes.js
+++ api/routes/chatbotRoutes.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
     //Defines simple route
     app.get('/', (req, res) => res.send('Chatbot API v0.0.1'));
     //todo: add routes to all endpoints
+
+    //Get all keywords
+    //app.get('/keywords', (req, res) => res.send('Chatbot API v0.0.2'));
 };
