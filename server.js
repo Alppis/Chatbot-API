8bebf77352996129f628f4156fd5649cd22ceb9d
Miikka Alaperä <miikka.alapera@student.oulu.fi>
2018-04-15 21:12:59 +0300

    Server.js updatet with Objection,Knex and error handling

--- server.js
+++ server.js
@@ -1,7 +1,143 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000;
+//Code from ObjectionJS documentation https://vincit.github.io/objection.js
+//and ES6 example project https://github.com/Vincit/objection.js/tree/master/examples/express-es6
 
-app.listen(port);
+const Knex = require('knex');
+const express = require('express');
+const bodyParser = require('body-parser');
+const promiseRouter = require('express-promise-router');
+const knexConfig = require('./knexfile');
+const { Model } = require('objection');
 
-console.log('chatbot RESTful API server started on: ' + port);
+//Knex initialization
+const knex = Knex(knexConfig.development);
+
+//Binding models for keywords, users, responses and statistics to a knex instance
+Model.knex(knex);
+
+const router = promiseRouter();
+const app = express()
+    .use(bodyParser.json())
+    .use(router)
+
+//Error handling
+const {
+    ValidationError,
+    NotFoundError
+  } = require('objection');
+  
+  const {
+    DBError,
+    ConstraintViolationError,
+    UniqueViolationError,
+    NotNullViolationError,
+    ForeignKeyViolationError,
+    CheckViolationError,
+    DataError
+  } = require('objection-db-errors');
+  
+  //`res` is an express response object.
+  function errorHandler(err, res) {
+    if (err instanceof ValidationError) {
+      switch (err.type) {
+        case 'ModelValidation':
+          res.status(400).send({
+            message: err.message,
+            type: 'ModelValidation',
+            data: err.data
+          });
+          break;
+        case 'RelationExpression':
+          res.status(400).send({
+            message: err.message,
+            type: 'InvalidRelationExpression',
+            data: {}
+          });
+          break;
+        case 'UnallowedRelation':
+          res.status(400).send({
+            message: err.message,
+            type: 'UnallowedRelation',
+            data: {}
+          });
+          break;
+        case 'InvalidGraph':
+          res.status(400).send({
+            message: err.message,
+            type: 'InvalidGraph',
+            data: {}
+          });
+          break;
+        default:
+          res.status(400).send({
+            message: err.message,
+            type: 'UnknownValidationError',
+            data: {}
+          });
+          break;
+      }
+    } else if (err instanceof NotFoundError) {
+      res.status(404).send({
+        message: err.message,
+        type: 'NotFound',
+        data: {}
+      });
+    } else if (err instanceof UniqueViolationError) {
+      res.status(409).send({
+        message: err.message,
+        type: 'UniqueViolation',
+        data: {
+          columns: err.columns,
+          table: err.table,
+          constraint: err.constraint
+        }
+      });
+    } else if (err instanceof NotNullViolationError) {
+      res.status(400).send({
+        message: err.message,
+        type: 'NotNullViolation',
+        data: {
+          column: err.column,
+          table: err.table,
+        }
+      });
+    } else if (err instanceof ForeignKeyViolationError) {
+      res.status(409).send({
+        message: err.message,
+        type: 'ForeignKeyViolation',
+        data: {
+          table: err.table,
+          constraint: err.constraint
+        }
+      });
+    } else if (err instanceof CheckViolationError) {
+      res.status(400).send({
+        message: err.message,
+        type: 'CheckViolation',
+        data: {
+          table: err.table,
+          constraint: err.constraint
+        }
+      });
+    } else if (err instanceof DataError) {
+      res.status(400).send({
+        message: err.message,
+        type: 'InvalidData',
+        data: {}
+      });
+    } else if (err instanceof DBError) {
+      res.status(500).send({
+        message: err.message,
+        type: 'UnknownDatabaseError',
+        data: {}
+      });
+    } else {
+      res.status(500).send({
+        message: err.message,
+        type: 'UnknownError',
+        data: {}
+      });
+    }
+  }
+
+//Listen for incoming requests
+const server = app.listen(3000, () => console.log('Chatbot API listening on port 3000'));
