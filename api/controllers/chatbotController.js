f7063b8664e9591c03c8dd4e92545d47c62925ac
Miikka Alaperä <miikka.alapera@student.oulu.fi>
2018-05-08 13:06:03 +0300

    Get single resource fixed

--- api/controllers/chatbotController.js
+++ api/controllers/chatbotController.js
@@ -9,7 +9,7 @@ const Statistics = require('../models/statisticsModel');
 
 module.exports = router => {
     //Get all keywords
-    router.get('/keywords', async (req, res) => {
+    router.get('/api/keywords', async (req, res) => {
         const keyword = await Keywords.query();
 
         if (!keyword) {
@@ -20,10 +20,10 @@ module.exports = router => {
     });
 
     //Get single keyword (may need changes to naming)
-    router.get('/keywords/:keyword', async (req, res) => {
+    router.get('/api/keywords/:keywordid', async (req, res) => {
         const keyword = await Keywords.query()
          .skipUndefined()
-         .where('keyword', req.query.keyword)
+         .where('keywordid', '=', req.params.keyword)
 
          res.send(keyword);
     });
@@ -31,10 +31,10 @@ module.exports = router => {
     //Modify single keyword //TODO\\
 
     //Remove single keyword (may need changes to naming)
-    router.delete('/keywords/:keyword', async (req, res) => {
+    router.delete('/api/keywords/:keyword', async (req, res) => {
         await Keywords.query()
         .delete()
-        .where('keyword', req.query.keyword)
+        .where('keyword', '=', req.params.keyword)
 
         res.send({});
     });
@@ -42,7 +42,7 @@ module.exports = router => {
     //Modify keywords and assiciated responses //TODO\\
 
     //Add new keyword
-    router.post('/keywords', async (req, res) => {
+    router.post('/api/keywords', async (req, res) => {
         const keyword = await Keywords.query();
 
         const addKeyword = await keyword.insert(req.body);
@@ -50,7 +50,7 @@ module.exports = router => {
     });
 
     //Get all responses
-    router.get('/responses', async (req, res) => {
+    router.get('/api/responses', async (req, res) => {
         const response = await Responses.query();
 
         if (!response) {
@@ -61,15 +61,12 @@ module.exports = router => {
     });
 
     //Get single response (may need changes to naming)
-    router.get('/responses/:id', async (req, res) => {
-        /*const response = await Responses.query().findById(req.params.id);*/
-        /***********************************/
-        /********NOT WORKING CORRECTLY******/
-        /***********************************/
+    router.get('/api/responses/:responseid', async (req, res) => {
 
-        const response = await Responses.query()
+        const response = await Responses
+         .query()
          .skipUndefined()
-         .where('responseid', '=', 'Responses.responseid');
+         .where('responseid', '=', req.params.responseid);
 
         if (!response) {
             throw createStatusCodeError(404);
@@ -79,7 +76,7 @@ module.exports = router => {
     });
 
     //Add new response
-    router.post('/responses', async (req, res) => {
+    router.post('/api/responses', async (req, res) => {
         const response = await Responses.query();
 
         const addResponse = await response.insert(req.body);
@@ -87,21 +84,21 @@ module.exports = router => {
     });
 
     //Delete single response
-    router.delete('/responses/:id', async (req, res) => {
+    router.delete('/api/responses/:id', async (req, res) => {
         await Keywords.query().deleteById(req.params.id);
 
         res.send({});
     });
 
     //Modify single response
-    router.patch('/responses/:id', async (req, res) => {
+    router.patch('/api/responses/:id', async (req, res) => {
         const response = await Responses.query().patchAndFetchById(req.params.id, req.body);
 
         res.send(response);
     });
 
     //Get statistics
-    router.get('/statistics', async (req, res) => {
+    router.get('/api/statistics', async (req, res) => {
         const statistic = await Statistics.query();
 
         if (!static) {
@@ -111,15 +108,8 @@ module.exports = router => {
         res.send(statistic);
     });
 
-    //Error handling. Handled in server.js
-    function createStatusCodeError(statusCode) {
-        return Object.assign(new Error(), {
-            statusCode
-        });
-    }
-
     //Get all users
-    router.get('/users', async (req, res) => {
+    router.get('/api/users', async (req, res) => {
         const user = await Users.query();
 
         if (!user) {
@@ -130,10 +120,10 @@ module.exports = router => {
     });
 
     //Get single user (may need changes to naming)
-    router.get('/users/:user', async (req, res) => {
+    router.get('/api/users/:user', async (req, res) => {
         const user = await Users.query()
          .skipUndefined()
-         .where('user', req.query.user)
+         .where('user', '=', req.params.user)
 
          res.send(user);
     });
@@ -141,7 +131,7 @@ module.exports = router => {
     //Modify single user //TODO\\
 
     //Remove single user (may need changes to naming)
-    router.delete('/users/:user', async (req, res) => {
+    router.delete('/api/users/:user', async (req, res) => {
         await Users.query()
         .delete()
         .where('user', req.query.keyword)
@@ -152,10 +142,17 @@ module.exports = router => {
     //Modify users //TODO\\
 
     //Add new user
-    router.post('/users', async (req, res) => {
+    router.post('/api/users', async (req, res) => {
         const user = await Users.query();
 
         const addUser = await user.insert(req.body);
         res.send(addUser);
     });
+
+    //Error handling. Handled in server.js
+    function createStatusCodeError(statusCode) {
+        return Object.assign(new Error(), {
+            statusCode
+        });
+    }
 }
